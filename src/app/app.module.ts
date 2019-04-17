e07c0be79e5925c4feafbbc358810bfee820b7b1
Eduardo Gonzalez <jose_e_gonzalez@homedepot.com>
2019-04-17 13:11:22 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { AdComponent } from './ad/ad.component';
+import { ShowViewComponent } from './show-view/show-view.component';
+import "angular2-navigate-with-data";
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdComponent,
+    ShowViewComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
